42908341320abd2c2be88fde0feae3f28dc1a607
dubell_g <dubell_g@epitech.eu>
2015-11-26 11:09:29 +0100

    Script to spawn youtubers randomly

---
+++ Youteamfight/Assets/Scripts/SpawnYoutubers.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnYoutubers : MonoBehaviour {
+
+	public	GameObject[]	YTBList;
+	private	GameObject[]	spawnList;
+	private	double			countdown;
+	private	int				randYtb;
+	private	int				randSpawn;
+
+	// Use this for initialization
+	void Start () 
+	{
+		countdown = 0.0f;
+		spawnList = GameObject.FindGameObjectsWithTag("Spawn");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		countdown += Time.deltaTime;
+		if (countdown >= 2.5f) 
+		{
+			countdown = 0.0f;
+			randYtb = (int)(Mathf.Round(Random.value));
+			randSpawn = (int)(Mathf.Round(Random.Range(0.0f, spawnList.Length - 1)));
+			Instantiate(YTBList[randYtb], spawnList[randSpawn].transform.position, Quaternion.identity);
+		}
+	}
+}
